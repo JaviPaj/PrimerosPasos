a8f99bf39bdd1ae0115db1d547af831fb4652886
Javier Pajarito <javipaj@gmail.com>
2021-06-10 23:07:20 -0500

    Se creó el archivo con las palabras a ser descubiertas, se escribio la línea de código para seleccionar una palabra al azar.

--- HangMan.py
+++ HangMan.py
@@ -1,7 +1,14 @@
-def hangman():
-  pass
+import random
 
+
+def hangman():
+    with open("./secrets_words.txt","r", encoding="UTF-8") as w:
+        target_word = random.choice(list(w)).strip().upper()
+        word_hidden = ["_"] * len(target_word)
+
+        print(target_word)
+        print(word_hidden)
 
 if __name__=="__main__":
-  hangman()
+    hangman()
   
