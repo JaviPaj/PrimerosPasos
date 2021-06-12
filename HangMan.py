b26b7940d88d3b9c5d0f70bd042047a2ba44a5fb
Javier Pajarito <javipaj@gmail.com>
2021-06-12 01:01:19 -0500

    Se incluyó la función para quitar acentos, se ajustó el final del ciclo while y se corrigió la limpieza de pantalla que no permitia cambiar underscores por letras.

--- HangMan.py
+++ HangMan.py
@@ -2,27 +2,28 @@ import random
 import os
 
 def word_select():
-    # This function will select a word from the list.
+    # This function will select a word from the list and make it capital letters & unaccented.
     with open("./secrets_words.txt","r", encoding="UTF-8") as w:
-        target_word = random.choice(list(w)).strip().upper()
-        # Aún esta el problema de quitar las tildes.
-
+        selected_word = random.choice(list(w)).strip().upper()
+        accents = selected_word.maketrans("ÁÉÍÓÚ","AEIOU")
+        target_word = selected_word.translate(accents)
+        
         letter_index_dict = {}
         for idx, letter in enumerate(target_word):
             if not letter_index_dict.get(letter): 
                 letter_index_dict[letter] = []
             letter_index_dict[letter].append(idx)
-    
+
+        # Transform the word in undersores, like hidden!.     
+        transf_to_underscore = ["_"] * len(target_word)
+
         while True:
-            # Transform the word in undersores, like hidden!.
-            transf_to_underscore = ["_"] * len(target_word)
             os.system("cls")
             print(f"¡Adivina la siguiente palabra de {len(target_word)} letras!")         
             print("\n")
             for element in transf_to_underscore:
                 print(element + "  ", end="")
             print("\n")
-            print(target_word)
 
             # Ask the letters to the gamer.
             letter = input("Digita una letra: ").upper()
@@ -31,15 +32,13 @@ def word_select():
             if letter in letter_index_dict:
                 for idx in letter_index_dict[letter]:
                     transf_to_underscore[idx] = letter
-                    print(transf_to_underscore)
-                else:
-                    continue
-
 
             if "_" not in transf_to_underscore:
                 os.system("cls")
+                print("\n")
                 print("¡Ganaste! La palabra era", target_word)
-            break
+                print("\n")
+                break
 
 
 
